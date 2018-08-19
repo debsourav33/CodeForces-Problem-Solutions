41350c48f89f415566537a119d38a4411a980ca8
debsourav33 <debsourav33@gmail.com>
2018-08-19 22:02:48 +0600

    Literally Nothing

---
+++ 110B- Lucky Strings.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+main(){
+    int n;
+    cin>>n;
+
+    while(1){
+        if(n==0) break;
+
+        cout<<"a";
+
+        n--;
+        if(n==0) break;
+
+        cout<<"b";
+
+        n--;
+        if(n==0) break;
+
+        cout<<"c";
+
+        n--;
+        if(n==0) break;
+
+        cout<<"d";
+
+        n--;
+    }
+
+    puts("");
+
+
+
+}
