a4233bc1394a3cc7c1a303551863bd7b293fcbe2
debsourav33 <debsourav33@gmail.com>
2018-09-14 16:00:43 +0600

    Ad-hoc

---
+++ 478C- Table Decoration.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//{
+#define cel(n,k) ((n-1)/k+1)
+#define sets(a) memset(a, -1, sizeof(a))
+#define clr(a) memset(a, 0, sizeof(a))
+#define max(a,b) ((a)>(b)? (a):(b))
+#define min(a,b) ((a)<(b)? (a):(b))
+#define fr(n) for(int i=0;i<n;i++)
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define mp make_pair
+#define ff first
+#define ss second
+#define INF 10000007
+
+typedef long l;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<long long,long long> pll;
+//}
+
+ll a[3], r, g, b, mini, maxi, ans;
+
+main(){
+    fr(3)
+    cin>>a[i];
+
+    sort(a,a+3);
+    ans= min(a[0]+a[1],(a[0]+a[1]+a[2])/3);
+
+    cout<<ans<<endl;
+}
