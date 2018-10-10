52e29201fbe252b9e35703efbfcbc5ef3811dde0
debsourav33 <debsourav33@gmail.com>
2018-10-10 21:56:21 +0600

    LYFT Cup

---
+++ 1033A- King Escape.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//{
+#define si(a) scanf("%d",&a)
+#define sii(a,b) scanf("%d %d",&a,&b);
+#define siii(a,b,c) scanf("%d %d %d",&a,&b,&c);
+
+#define sl(a) scanf("%lld",&a)
+#define sll(a,b) scanf("%lld %lld",&a,&b);
+#define slll(a,b,c) scanf("%lld %lld %lld",&a,&b,&c);
+
+#define cel(n,k) ((n-1)/k+1)
+#define sets(a) memset(a, -1, sizeof(a))
+#define clr(a) memset(a, 0, sizeof(a))
+#define max(a,b) ((a)>(b)? (a):(b))
+#define min(a,b) ((a)<(b)? (a):(b))
+#define fr(n) for(int i=0;i<n;i++)
+#define fr1(n) for(int i=1;i<=n;i++)
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define mp make_pair
+#define ff first
+#define ss second
+#define INF 10000007
+#define fastIO() ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+typedef long long i64;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<long long,long long> pll;
+//}
+
+i64 n, a1, a2, b1, b2, t1, t2;
+
+main(){
+    sl(n);
+
+    cin>>a1>>a2>>b1>>b2>>t1>>t2;
+    int flag= 0;
+
+    if((b1<a1 && t1<a1) || (b1>a1 && t1>a1)){
+
+        if((b2<a2 && t2<a2) || (b2>a2 && t2>a2))
+            flag= 1;
+    }
+
+    if(flag)
+        cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
+}
