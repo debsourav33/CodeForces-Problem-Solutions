a592a95735b1961e5f01cf50beed3f5589b2372f
debsourav33 <debsourav33@gmail.com>
2018-10-25 01:14:20 +0600

    Codeforces Round 518

---
+++ 1068A- Birthday.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//{
+#define si(a) scanf("%d",&a)
+#define sii(a,b) scanf("%d %d",&a,&b);
+#define siii(a,b,c) scanf("%d %d %d",&a,&b,&c);
+
+#define sl(a) scanf("%lld",&a)
+#define sll(a,b) scanf("%lld %lld",&a,&b);
+#define slll(a,b,c) scanf("%lld %lld %lld",&a,&b,&c);
+
+#define cel(n,k) ((n-1)/k+1)
+#define sets(a) memset(a, -1, sizeof(a))
+#define clr(a) memset(a, 0, sizeof(a))
+#define max(a,b) ((a)>(b)? (a):(b))
+#define min(a,b) ((a)<(b)? (a):(b))
+#define fr(n) for(int i=0;i<n;i++)
+#define fr1(n) for(int i=1;i<=n;i++)
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define mp make_pair
+#define ff first
+#define ss second
+#define INF 10000007
+#define fastIO() ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+typedef long long i64;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<long long,long long> pll;
+//}
+
+ull n, m, k, l;
+
+main(){
+    cin>>n>>m>>k>>l;
+
+    if(n<m || (l+k)>n){
+        cout<<"-1"<<endl;
+        return 0;
+    }
+
+    ull ans= cel(l+k,m);
+
+    if(m*ans>n)
+        cout<<"-1"<<endl;
+    else
+        cout<<ans<<endl;
+}
---
+++ 1068C- Colored Rooks.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//{
+#define si(a) scanf("%d",&a)
+#define sii(a,b) scanf("%d %d",&a,&b);
+#define siii(a,b,c) scanf("%d %d %d",&a,&b,&c);
+
+#define sl(a) scanf("%lld",&a)
+#define sll(a,b) scanf("%lld %lld",&a,&b);
+#define slll(a,b,c) scanf("%lld %lld %lld",&a,&b,&c);
+
+#define cel(n,k) ((n-1)/k+1)
+#define sets(a) memset(a, -1, sizeof(a))
+#define clr(a) memset(a, 0, sizeof(a))
+#define max(a,b) ((a)>(b)? (a):(b))
+#define min(a,b) ((a)<(b)? (a):(b))
+#define fr(n) for(int i=0;i<n;i++)
+#define fr1(n) for(int i=1;i<=n;i++)
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define mp make_pair
+#define ff first
+#define ss second
+#define INF 10000007
+#define fastIO() ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+typedef long long i64;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<long long,long long> pll;
+//}
+
+i64 n, m, a, b, nxt=1e8;
+vector<int> v[105];
+main(){
+    sll(n,m);
+
+    fr(m){
+        sll(a,b);
+
+        v[a].pb(nxt);
+        v[b].pb(nxt);
+        --nxt;
+    }
+
+    fr1(n){
+        printf("%d\n",v[i].size()+1);
+        printf("%d %d\n",i,i);
+
+        for(int j=0;j<v[i].size();j++){
+            printf("%d %d\n",i,v[i][j]);
+        }
+    }
+}
