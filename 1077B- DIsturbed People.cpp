74719f42f674b096921e350f8110e7b4886c2409
debsourav33 <debsourav33@gmail.com>
2018-11-17 11:18:55 +0600

    Codeforces Round 521

---
+++ 1077B- DIsturbed People.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//{
+#define si(a) scanf("%d",&a)
+#define sii(a,b) scanf("%d %d",&a,&b);
+#define siii(a,b,c) scanf("%d %d %d",&a,&b,&c);
+
+#define sl(a) scanf("%lld",&a)
+#define sll(a,b) scanf("%lld %lld",&a,&b);
+#define slll(a,b,c) scanf("%lld %lld %lld",&a,&b,&c);
+
+#define outi(a) printf("%d\n",a)
+#define outii(a,b) printf("%d %d\n",a,b)
+#define outis(a) printf(" %d",a)
+
+#define outl(a) printf("%lld\n",a)
+#define outll(a,b) printf("%lld %lld\n",a,b)
+#define outls(a) printf(" %lld",a)
+
+#define cel(n,k) ((n-1)/k+1)
+#define sets(a) memset(a, -1, sizeof(a))
+#define clr(a) memset(a, 0, sizeof(a))
+#define max(a,b) ((a)>(b)? (a):(b))
+#define min(a,b) ((a)<(b)? (a):(b))
+#define fr(n) for(int i=0;i<n;i++)
+#define fr1(n) for(int i=1;i<=n;i++)
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define mp make_pair
+#define ff first
+#define ss second
+#define INF 10000007
+#define fastIO() ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+typedef long long i64;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<long long,long long> pll;
+//}
+
+int a[105], prevs, mark[105], k=0;
+
+int main(){
+    clr(mark);
+    int n;
+    si(n);
+
+    fr1(n)
+        si(a[i]);
+
+    prevs= 0;
+    fr1(n){
+        if(a[i]==0 && prevs==1){
+            mark[i]= 1;
+        }
+        if(a[i] && mark[i-1]){
+            k++;
+            prevs= 0;
+        }
+        else
+            prevs= a[i];
+    }
+
+    printf("%d\n",k);
+}
