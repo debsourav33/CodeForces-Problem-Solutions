5f386668e67b16effa1852acc656dc81f5497042
debsourav33 <debsourav33@gmail.com>
2019-04-06 14:49:56 +0600

    DFS, Brute Force

---
+++ 1130C - Connect.cpp
@@ -0,0 +1,127 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+//{
+#define si(a) scanf("%d",&a)
+#define sii(a,b) scanf("%d %d",&a,&b);
+#define siii(a,b,c) scanf("%d %d %d",&a,&b,&c);
+
+#define sl(a) scanf("%lld",&a)
+#define sll(a,b) scanf("%lld %lld",&a,&b);
+#define slll(a,b,c) scanf("%lld %lld %lld",&a,&b,&c);
+
+#define outi(a) printf("%d\n",a)
+#define outii(a,b) printf("%d %d\n",a,b)
+#define outis(a) printf(" %d",a)
+
+#define outl(a) printf("%lld\n",a)
+#define outll(a,b) printf("%lld %lld\n",a,b)
+#define outls(a) printf(" %lld",a)
+
+#define cel(n,k) ((n-1)/k+1)
+#define sets(a) memset(a, -1, sizeof(a))
+#define clr(a) memset(a, 0, sizeof(a))
+#define max(a,b) ((a)>(b)? (a):(b))
+#define min(a,b) ((a)<(b)? (a):(b))
+#define fr(n) for(int i=0;i<n;i++)
+#define fr1(n) for(int i=1;i<=n;i++)
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define mp make_pair
+#define ff first
+#define ss second
+#define INF 10000007
+#define fastIO() ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+typedef long long i64;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<long long,long long> pll;
+//}
+
+
+int vis[55][55], water[55][55], n, edges, cycle=0;
+
+int dr[]= {1,0,-1,0};
+int dc[]= {0,1,0,-1};
+
+vector<pii> reach_s, reach_d;
+
+int isValid(int x, int y){
+    return !vis[x][y] && x>=1 && y>=1 && x<=n && y<=n && !water[x][y];
+}
+
+void dfs(int x, int y){
+    reach_s.pb(mp(x,y));
+    vis[x][y]= 1;
+
+    fr(4) {
+        pii nd = mp(x + dr[i], y+dc[i]);
+
+        if (isValid(nd.ff,nd.ss)) {
+            vis[nd.ff][nd.ss]= 1;
+            dfs(nd.ff,nd.ss);
+        }
+    }
+}
+
+void ddfs(int x, int y){
+    reach_d.pb(mp(x,y));
+    vis[x][y]= 1;
+
+    fr(4) {
+        pii nd = mp(x + dr[i], y+dc[i]);
+
+        if (isValid(nd.ff,nd.ss)) {
+            vis[nd.ff][nd.ss]= 1;
+            ddfs(nd.ff,nd.ss);
+        }
+    }
+}
+
+int getBridge(){
+    int mini= 1e9;
+
+    for(pii s: reach_s){
+        for(pii d: reach_d){
+            int dist= (s.ff-d.ff)*(s.ff-d.ff)+ (s.ss-d.ss)*(s.ss-d.ss);
+
+            if(!dist)  return 0;
+
+            mini= min(mini,dist);
+        }
+    }
+
+    return mini;
+}
+
+main(){
+    int a, b, x, y;
+
+    si(n);
+
+    sii(a,b);
+    sii(x,y);
+
+    fr1(n){
+        for(int j=1;j<=n;j++){
+            scanf("%1d",&water[i][j]);
+        }
+    }
+
+
+    clr(vis), dfs(a,b);
+    clr(vis), ddfs(x,y);
+
+    /*fr(reach_s.size())
+        outii(reach_s[i].ff,reach_s[i].ss);
+
+    puts("----");
+
+    fr(reach_d.size())
+        outii(reach_d[i].ff,reach_d[i].ss);*/
+
+    outi(getBridge());
+}
+
